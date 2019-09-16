78c364b103de67d30e7837717e8c7d77926efc77
jorgenlundegard <jorgenlundegard@gmail.com>
2019-09-16 18:24:10 +0200

    Merge branch 'master' of C:\Users\jorge\IdeaProjects\Oblig1 with conflicts.

--- src/oblig1/Oblig1.java
+++ src/oblig1/Oblig1.java
@@ -8,13 +8,10 @@ import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import javax.security.auth.login.AccountExpiredException;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+
 
 
 public class Oblig1 {
@@ -37,8 +34,7 @@ public class Oblig1 {
 
 
         //Oppgave 10
-        System.out.println(Oblig1.inneholdt("ABBA", "ABBA"));
-
+        System.out.println(Oblig1.inneholdt("ØÅÅØ", "ØØÅØØ"));
     }
 
     public static void quickSort(int a[], int fra, int til) {
@@ -359,7 +355,6 @@ public class Oblig1 {
         int tempHoldIndex;
         int[] index = new int[a.length];
         int[] help = Arrays.copyOf(a, a.length);
-        boolean jobb = false;
 
         //populerer index med nåværende indeksposisjoner
         for(int i = 0; i<a.length; i++){index[i] = i;}
@@ -375,9 +370,8 @@ public class Oblig1 {
 
                 help[i] = tempHold;
                 index[i] = tempHoldIndex;
-                jobb = true;
+                i = 0; //Hvis en operasjon utføres, starter loopen paa nytt.
             }
-            if(jobb){i = 0; jobb=false;} //hvis en jobb utføres starter loopen paa nytt. Ellers er vi ferdige.
         }
         return index;
         //throw new NotImplementedException();
@@ -441,94 +435,11 @@ public class Oblig1 {
 
     ///// Oppgave 10 //////////////////////////////////////
     public static int bokstavNr(char bokstav) {
-        return (int) bokstav;
-    }
-
-    public static int tellBokstaver(char[] chars, char c){
-        int antall = 0;
-        if(chars.length == 0){return 0;}
-
-        for(int i = 0; i<chars.length; i++){
-            if(chars[i] == c){
-                antall ++;
-            }
-        }
-        return antall;
+        return Character.getNumericValue(bokstav);
     }
 
     public static boolean inneholdt(String a, String b) {
-        String countedString;
-        int counted = 0;
-        char[] countedChars = new char[30];
-        char[] charsA = a.toCharArray();
-        char[] charsB = b.toCharArray();
 
-        if(a.isEmpty()){return true;}
-        else if(!a.isEmpty() && b.isEmpty()){return false;}
-
-        for (char c: charsA) {
-            countedString = Arrays.toString(countedChars);
-            if (countedString.contains(new String(new char[]{c}))) {
-                //gaa videre
-            } else {
-                if (tellBokstaver(charsA, c) > tellBokstaver(charsB, c)) {
-                    return false;
-                } else{
-                    countedChars[counted] = c;
-                    counted ++;
-                }
-            }
-        }
-        return true;
-
-        /* MADS TULL
-        //spesialtilfeller
-        if(a.isEmpty()){return true;}
-        else if(!a.isEmpty() && b.isEmpty()){return false;}
-        int n = 0;
-        int bStart = 0;
-        int[] aInts = stringToInts(a);
-        int[] bInts = stringToInts(b);
-        ArrayList<int[]> A = new ArrayList<>();
-        ArrayList<int[]> B = new ArrayList<>();
-        boolean hasOneLetter = false;
-
-        quickSort(aInts, 0, aInts.length - 1);
-        quickSort(bInts, 0, bInts.length - 1);
-
-        if(bInts.length < aInts.length){return false;}
-
-        A.add(tellBokstaver(aInts, 0));
-        B.add(tellBokstaver(bInts, 0));
-        for(int i = 1; i<aInts.length; i++){
-            if(aInts[i-1] != aInts[i]){
-                A.add(tellBokstaver(aInts, i));
-            }
-        }
-        for(int i = 1; i<bInts.length; i++){
-            if(bInts[i-1] != bInts[i]){
-                B.add(tellBokstaver(bInts, i));
-            }
-        }
-
-        /*for(int i = 0; i<A.size(); i++){
-            for(int j = 0; j<B.size(); j++){
-                if(A.get(i)[0]==B.get(j)[0]){
-                    hasOneLetter = true;
-                    if(A.get(i)[1] > B.get(j)[1]){return false;}
-                }
-            }
-            if(!hasOneLetter){return false;}
-        }
-        return true;
-
-        ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-        */
-
-
-
-
-        /*
         StringBuilder bb = new StringBuilder(b);    // Flytter b over i en stringbuilder hvor man kan slette objekter.
         int antallA = a.length();
         int antallB = b.length();
@@ -541,7 +452,7 @@ public class Oblig1 {
 
             for (int j = 0; j < antallB; j++) {     // Gaar gjennom b for aa finne gjeldene bokstav fra a
                 if (bokstav == bb.charAt(j)){       // Hvis gjeldende bokstav fra a er aa finne i b
-                    bb.deleteCharAt(j);             // slettes denne bokstavan fra b slik at den ikke lenger er i veien mtp to av samme bokstav.
+                    bb.deleteCharAt(j);             // sletter denne bokstavan fra b slik at den ikke lenger er i veien mtp to av samme bokstav.
                     teller++;
                     antallB--;
                     break;
@@ -552,7 +463,6 @@ public class Oblig1 {
         if (teller == antallA) {return true;}       // Hvis teller har samme antall som verdier i a er alle funnet og returnerer folgelig true.
         else {return false;}
 
-        */
 
         /*
         char gjeldendeBokstav;
